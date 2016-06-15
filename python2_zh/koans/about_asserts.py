c3e14d5ee1d3a18027e052f9623b2f710bd1f1d4
牛棚琐思 <viprs@qq.com>
2016-06-15 21:13:38 +0800

    添加assert的中文说明

--- python2_zh/koans/about_asserts.py
+++ python2_zh/koans/about_asserts.py
@@ -11,48 +11,48 @@ class AboutAsserts(Koan):
         We shall contemplate truth by testing reality, via asserts.
         """
 
-        # Confused? This video should help:
+        # 如果你感到困惑，下面的视频可能会有所帮助（需要科学上网）
         #
         #   http://bit.ly/about_asserts
 
-        self.assertTrue(True)  # This should be True
+        self.assertTrue(True)  # 这里应该填 True
 
     def test_assert_with_message(self):
         """
-        Enlightenment may be more easily achieved with appropriate messages.
+        assertTrue可以在第二个参数添加一些提示，帮助你发现错误
         """
-        self.assertTrue(True)
+        self.assertTrue(False, "这里应该填True -- 请修正")
 
     def test_fill_in_values(self):
         """
-        Sometimes we will ask you to fill in the values
+        有时候，你需要填写一些数值，占位符一般是__
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, __)
 
     def test_assert_equality(self):
         """
-        To understand reality, we must compare our expectations against
-        reality.
+        为了验证实际值与期望值，我们必须将两者比较
         """
-        expected_value = 2
+        expected_value = __
         actual_value = 1 + 1
         self.assertTrue(expected_value == actual_value)
 
     def test_a_better_way_of_asserting_equality(self):
         """
-        Some ways of asserting equality are better than others.
+        有些情况，assertEqual比assertTrue更适合做比较
         """
-        expected_value = 2
+        expected_value = __
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
 
     def test_that_unittest_asserts_work_the_same_way_as_python_asserts(self):
         """
-        Understand what lies within.
+        验证表达式是否为真
+        格式: assert 表达式
         """
 
-        # This throws an AssertionError exception
+        # assert 如果是False会抛出一个 AssertionError 异常
         assert True
 
     def test_that_sometimes_we_need_to_know_the_class_type(self):
@@ -72,8 +72,4 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(str, "navel".__class__) # It's str, not <type 'str'>
-
-        # Need an illustration? More reading can be found here:
-        #
-        #   http://bit.ly/__class__
+        self.assertEqual(__, "navel".__class__) # It's str, not <type 'str'>
